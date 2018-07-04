8621a0b117ef032f78b992e56e19c33d31a84c46
Kenny Lov <lov.kenny1996@gmail.com>
2018-07-03 19:08:05 -0700

    updating

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/kenny/Desktop/flask-practice/venv"
+set -gx VIRTUAL_ENV "/home/kenny/Dropbox/flask-practice/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
