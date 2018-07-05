bcc41097a2f49ff57fba60c46a8415572cb346cd
Kenny Lov <lov.kenny1996@gmail.com>
2018-07-05 00:56:16 -0700

    changed css

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,3 @@
 const element = document.querySelector('h2');
 
-element.innerHTML = element.innerHTML.replace('.', '<span style="color: #4285f4;">.</span>');
+element.innerHTML = element.innerHTML.replace('.', '<span style="color: #4285f4; font-size: 70px;" >.</span>');
